3e1f4e2f2939e68b52494e9962774bbaa5be9f3c
Richard Flagg <richard.lee.flagg@outlook.com>
2023-12-11 11:03:16 -0500

    Debugging environmental issue

--- tests/FACDataMinerAPI.Tests/EnvironmentalTests.cs
+++ tests/FACDataMinerAPI.Tests/EnvironmentalTests.cs
@@ -10,7 +10,7 @@ public class EnvironmentalTests
         
         Console.WriteLine($"Is token null? : {string.IsNullOrEmpty(token)}");
         Console.WriteLine($"Is token length ok? : {token.Length}");
-        Console.WriteLine($"Is first char ok? : {token[0] == 'q'}");
+        Console.WriteLine($"Is first char ok? : {token[39] == 'q'}");
         
         Assert.That(TestHelpers.GetAPIKey(), Is.Not.Null);
         Assert.That(TestHelpers.GetAPIKey(), Has.Length.EqualTo(40));
