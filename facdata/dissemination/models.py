a1b8e489c296e04f75280b6af0185df2dd499982
Rick Flagg <richard.lee.flagg@outlook.com>
2024-05-02 12:38:41 -0400

    adding requirements

--- facdata/dissemination/models.py
+++ facdata/dissemination/models.py
@@ -357,3 +357,38 @@ class gsa_general(models.Model):
     is_public  = models.BooleanField
     data_source = models.CharField
 
+
+class gsa_award(models.Model):
+
+    class Meta:
+        managed = True
+        db_table = "gsa_award"
+
+
+
+    report_id text,
+    auditee_uei text,
+    audit_year smallint,
+    award_reference text,
+    federal_agency_prefix text,
+    federal_award_extension text,
+    additional_award_identification text,
+    federal_program_name text,
+    amount_expended text,
+    cluster_name text,
+    other_cluster_name text,
+    state_cluster_name text,
+    cluster_total money,
+    federal_program_total money,
+    is_major bool,
+    is_loan bool,
+    loan_balance text,
+    is_direct bool,
+    audit_report_type text,
+    findings_count int,
+    is_passthrough_award bool,
+    passthrough_amount money
+
+
+
+
